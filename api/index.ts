a41b8665cf2fbce3ddc99127a1c76581eb951198
Noor Alam <noor.qualysec@gmail.com>
2026-03-27 17:47:11 +0530

    remove api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,10 @@
 // import app from "../src/server.js";
 
 // export default app;
-import serverless from "serverless-http";
-import app from "../src/server";
+// import serverless from "serverless-http";
+// import app from "../src/server";
 
-export default serverless(app);
+// export default serverless(app);
+import app from "../src/server.js";
+
+export default app;
